c7de1394f55d5a8acf5e73275b090bcd3a925459
dinesh.sutrix <dineshkumar.radhakrishnan@sutrixsolutions.com>
2022-06-14 17:00:52 +0530

    Initial commit

---
+++ src/main/java/com/example/springassesment/PersonService.java
@@ -0,0 +1,38 @@
+package com.example.springassesment;
+
+
+import com.example.springassesment.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    public Person createPerson(Person person) {
+        return personRepository.save(person);
+    }
+
+
+    public List<Person> getAllUsers(int age) {
+        List<Person> persons = (List<Person>) personRepository.findAll();
+        return persons.stream().filter(person -> person.getAge() > 30).collect(Collectors.toList());
+    }
+
+    public List<Person> getAllUsers() {
+        List<Person> persons = (List<Person>) personRepository.findAll();
+        return persons;
+    }
+
+
+    public Person getUserById(Long id) {
+        return personRepository.findById(id).orElse(null);
+    }
+
+
+}
---
+++ src/main/java/com/example/springassesment/model/Person.java
@@ -0,0 +1,68 @@
+package com.example.springassesment.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    @Id @GeneratedValue
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String email, String firstName, String lastName, int age) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
