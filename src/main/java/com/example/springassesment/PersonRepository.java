54d7aff0a983a3efb9c1b29093fa5efad7f9c9e2
dinesh.sutrix <dineshkumar.radhakrishnan@sutrixsolutions.com>
2022-06-15 10:09:02 +0530

    Converted into maven

--- src/main/java/com/example/springassesment/PersonRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.springassesment;
-
-import com.example.springassesment.model.Person;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person,Long> {}
